804ff734cf75b95e72efceba559dfd4a1282e026
Saeed S. Lafmejani <113765583+ssldk83@users.noreply.github.com>
2025-04-15 20:32:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -49,5 +49,8 @@ def get_data():
         "y": result[y]
     })
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT automatically
+    app.run(host="0.0.0.0", port=port)
