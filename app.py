8d96308ac3e90fb6faa7b114fabd31682927b997
Saeed S. Lafmejani <113765583+ssldk83@users.noreply.github.com>
2025-04-18 21:07:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,24 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for
 from fmpy import simulate_fmu
 from fmpy.util import plot_result
 import os
-import uuid
-import matplotlib.pyplot as plt
 
 app = Flask(__name__)
-UPLOAD_FOLDER = 'uploads'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        fmu = request.files['fmu']
-        fmu_filename = os.path.join(UPLOAD_FOLDER, str(uuid.uuid4()) + '.fmu')
-        fmu.save(fmu_filename)
-
-        result = simulate_fmu(fmu_filename)
-        fig = plot_result(result)
-
-        plot_path = os.path.join('static', 'result_plot.html')
-        fig.write_html(plot_path)  # Use this if you're using plotly
-
-        return redirect(url_for('view_plot'))
-
     return render_template('index.html')
+
+@app.route('/simulate')
+def simulate():
+    fmu_path = 'Rectifier.fmu'  # FMU file in the main folder
+    result = simulate_fmu(fmu_path)
+    
+    fig = plot_result(result)
+    plot_path = os.path.join('static', 'result_plot.html')
+    fig.write_html(plot_path)
+
+    return redirect(url_for('view_plot'))
 
 @app.route('/plot')
 def view_plot():
