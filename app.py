bbec951f3dbbc06766dd75a56d312c08d437b958
Saeed S. Lafmejani <113765583+ssldk83@users.noreply.github.com>
2025-05-03 16:28:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template
-
-################################### import apps
-from custominput_app import custominput_bp
-from nh3balance_app import nh3balance_bp
 from flask_cors import CORS
 
 ################################### run flas app
 app = Flask(__name__)
 CORS(app)  # Add this to enable CORS globally
 
+################################### import apps
+from custominput_app import custominput_bp
+from nh3balance_app import nh3balance_bp
+
 ################################### rout to the apps
-app.register_blueprint(landing_pages_bp, url_prefix=')
 app.register_blueprint(custominput_bp, url_prefix='/custominput')
 app.register_blueprint(nh3balance_bp, url_prefix='/nh3balance')
 
