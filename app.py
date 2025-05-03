7585f15628e035099195b8e7056f2fdbadc1561b
Saeed S. Lafmejani <113765583+ssldk83@users.noreply.github.com>
2025-05-04 00:47:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,15 +13,6 @@ from nh3balance_app import nh3balance_bp
 app.register_blueprint(custominput_bp, url_prefix='/custominput')
 app.register_blueprint(nh3balance_bp, url_prefix='/nh3balance')
 
-#################################### Landing page routes
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
 #################################### Port
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
