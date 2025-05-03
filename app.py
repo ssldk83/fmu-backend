d60a5b3934079c3a6a87a0f3fa6673a938733b41
Saeed S. Lafmejani <113765583+ssldk83@users.noreply.github.com>
2025-05-03 02:06:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from custominput_app import custominput_bp
+from nh3balance_app import nh3balance_bp
 
 app = Flask(__name__)
 app.register_blueprint(custominput_bp, url_prefix='/custominput')
+app.register_blueprint(nh3balance_bp, url_prefix='/nh3balance')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
