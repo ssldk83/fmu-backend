e19282a7e19162a9b923a6bd9c60ff297718acf4
Saeed S. Lafmejani <113765583+ssldk83@users.noreply.github.com>
2025-05-15 19:45:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ CORS(app)
 ################################### import apps
 from custominput_app import custominput_bp
 from nh3balance_app import nh3balance_bp
-from oandm_app import oandm_bp
+from heatpump_app import heatpump_bp
 
 ################################### rout to the apps
 app.register_blueprint(custominput_bp, url_prefix='/custominput')
 app.register_blueprint(nh3balance_bp, url_prefix='/nh3balance')
-app.register_blueprint(oandm_bp, url_prefix='/oandm')
+app.register_blueprint(heatpump_bp, url_prefix='/heatpump')
 
 #################################### Port
 if __name__ == "__main__":
