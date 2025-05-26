b07c8077542ff3b134f22e2f88b9ae350891a65d
Saeed S. Lafmejani <113765583+ssldk83@users.noreply.github.com>
2025-05-26 22:31:31 +0200

    Create lcoh_calc_app.py

---
+++ lcoh_calc_app.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, request, jsonify
+from flask_cors import CORS
+from pyH2A.Project import Project
+
+lcoh_bp = Blueprint('lcoh_bp', __name__)
+CORS(lcoh_bp)
+
+@lcoh_bp.route('/lcoh', methods=['GET'])
+def calculate_lcoh():
+    try:
+        capex = float(request.args.get('capex', 1000))
+        opex = float(request.args.get('opex', 3))
+        elec_price = float(request.args.get('elec_price', 50))
+        elec_consumption = float(request.args.get('elec_consumption', 50))
+        efficiency = float(request.args.get('efficiency', 70))
+
+        overrides = {
+            'Installed Capacity [MW]': 10,
+            'Electrolyzer CAPEX [$/kW]': capex,
+            'Fixed OPEX [% of CAPEX]': opex,
+            'Electricity Price [$/MWh]': elec_price,
+            'Electricity Consumption [kWh/kg]': elec_consumption,
+            'Efficiency [%]': efficiency
+        }
+
+        p = Project('PEM Electrolysis', override_parameters=overrides)
+        lcoh = p.get_LCOH()
+
+        return jsonify({
+            'status': 'success',
+            'inputs': overrides,
+            'lcoh': round(lcoh, 2)
+        })
+
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)})
